cde58ce016494682be420011fd8b91a948dfae76
Shubhadeep Roychowdhury <shubhadeeproychowdhury@gmail.com>
2020-11-16 09:18:41 +0100

    WIP commit

--- src/editor.rs
+++ src/editor.rs
@@ -2,9 +2,15 @@ use crate::Terminal;
 use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
+    cursor_position: Position,
 }
 
 impl Editor {
@@ -24,22 +30,34 @@ impl Editor {
     pub fn default() -> Self {
         Self { 
             should_quit: false,
-            terminal: Terminal::default().expect("There is an error to start the terminal"),
+            terminal: Terminal::default().expect("There is an error to initialize Tiny"),
+            cursor_position: Position {x: 0, y: 0},
          }
     }
 
     fn refresh_screen(&self) -> Result <(), std::io::Error> {
-        Terminal::cusrsor_position(0, 0);
+        Terminal::cusrsor_position(&Position{x: 0, y: 0});
         Terminal::cursor_hide();
         if self.should_quit {
             Terminal::clear_screen();
             print!("Goodbye!\r");
         } else {
             self.darw_row_nums();
-            Terminal::cusrsor_position(0, 0);
+            Terminal::cusrsor_position(&self.cursor_position);
         }
         Terminal::cusrsor_show();
         Terminal::flush()
+    }
+
+    fn draw_welcome_message(&self) {
+        let mut message = format!("Tiny Editor Version - {}", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        message = format!("~{}{}", spaces, message);
+        message.truncate(width);
+        println!("{}\r", message);
     }
 
     fn darw_row_nums(&self) {
@@ -47,7 +65,7 @@ impl Editor {
         for row in 0 .. height - 1 {
             Terminal::clear_current_line();
             if row == height / 3 {
-                println!("Tiny editor -- version {}\r", VERSION)
+                self.draw_welcome_message();
             } else {
                 println!("~\r");
             }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,17 @@ mod terminal;
 
 use editor::Editor;
 pub use terminal::Terminal;
+use editor::Position;
+
+use std::{panic, thread};
+use std::time::Duration;
 
 fn main() {
-    Editor::default().run();
+    let result = panic::catch_unwind(||{
+        Editor::default().run();
+    });
+
+    if result.is_err() {
+        thread::sleep(Duration::from_secs(3));
+    }
 }
